6a822ef8f4f119600b5a61df8fec296414e9c462
iGoliath <mdajs60@gmail.com>
2024-12-08 20:03:38 -0500

    Removed unnecessary table

--- Modified DES/Main.cpp
+++ Modified DES/Main.cpp
@@ -20,11 +20,6 @@ std::string DESEncrypt(std::string Plaintext, int* key);
 std::string DESDecrypt(std::string Ciphertext, int* key);
 void DESBruteForce(std::string Ciphertext, std::string plaintext);
 
-const int IPTable[64] = { 57, 49, 41, 33, 25, 17, 9, 1, 59, 51, 43, 35,
-27, 19, 11, 3, 61, 53, 45, 37, 29, 21, 13, 5, 63, 55, 47, 39, 31, 23, 15,
-7, 56, 48, 40, 32, 24, 16, 8, 0, 58, 50, 42, 34, 26, 18, 10, 2, 60, 52, 44,
-36, 28, 20, 12, 4, 62, 54, 46, 38, 30, 22, 14, 6 };
-
 const int PC2Table[48] = { 13, 16, 10, 23, 0, 4, 2, 27, 
 14, 5, 20, 9, 22, 18, 11, 3, 25, 7, 15, 6, 26, 19, 12,
 1, 40, 51, 30, 36, 46, 54, 29, 39, 50, 44, 32, 47, 43, 
